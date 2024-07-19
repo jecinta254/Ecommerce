f09d24b09ef856d642eb4a5ad2801a52de0535a2
jecinta254 <njokijecinta254@gmail.com>
2024-07-19 11:00:21 +0300

    SilverTech

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import { Navbar } from './Components/Navbar/Navbar';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import { Shop } from './Pages/Shop';
 import { ShopCategory } from './Pages/ShopCategory';
-import { Product } from './Pages/Product';
+import  { Product } from './Pages/Product';
 import { LoginSignup } from './Pages/LoginSignup';
 import { Cart } from './Pages/Cart';
 import { Footer }  from './Components/Footer/Footer'
@@ -17,16 +17,15 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-
       <Navbar/>
       <Routes>
-
         <Route path='/' element={<Shop/>}/>
         <Route path='/websites' element={<ShopCategory banner={men_banner} category="men"/>}/>
         <Route path='/softwares' element={<ShopCategory banner={women_banner} category="women"/>}/>
         <Route path='/hosting' element={<ShopCategory banner={kid_banner} category="kid"/>}/>
-        <Route path='/product' element={<Product/>}/>
-        <Route path=':productId' element={<Product/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=':productId' element={<Product/>}/>
+        </Route>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignup/>}/>
 
--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -29,6 +29,27 @@ export const ProductDisplay = (props) => {
                 <img src={star_dull_icon} alt="" />
                 <p>(122)</p>
             </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">KSH.{product.old_price}</div>
+                <div className="productdisplay-right-price-new">KSH.{product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas veritatis inventore similique quasi non sed provident expedita illum dolorum ipsum. Autem quas laboriosam, doloremque quae itaque laudantium eos obcaecati soluta!
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select Size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div>S</div>
+                    <div>M</div>
+                    <div>L</div>
+                    <div>XL</div>
+                    <div>XXL</div>
+
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className='productdisplay-right-category'><span>Category :</span>Women , T-Shirt, Crop Top</p>
+            <p className='productdisplay-right-category'><span>Tag :</span>Modern, Latest</p>
         </div>
     </div>
   )
