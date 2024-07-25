44b55488320ecf29ae6a86af4d12d6d9d38762fb
jecinta254 <njokijecinta254@gmail.com>
2024-07-25 10:14:56 +0300

    SilverTech

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus necessitatibus officia non quo numquam. Quisquam eum velit non earum, dicta labore deleniti a laboriosam maxime soluta necessitatibus aliquam, quibusdam quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quasi ut, maxime rem ipsa consectetur ex earum molestiae maiores aut officia architecto est, culpa provident, voluptate totam et! Magni, voluptatibus?
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa iste commodi facere in vel iure est perferendis nam enim. Inventore accusamus tempore necessitatibus doloribus quaerat! Neque consequatur quidem vitae ducimus.
+                </p>
+            </div>
+        
+    </div>
+  )
+}
