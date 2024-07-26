7d315bbc720b576eef7e67edae5a67b648ca3995
jecinta254 <njokijecinta254@gmail.com>
2024-07-26 14:45:09 +0300

    silvertech

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     <div className='footer'>
         <div className="footer-logo">
             <img src={footer_logo} alt="" />
-            <p>SHOPPER</p>
+            <p>Tech</p>
         </div>
         <ul className='footer-links'>
             <li>Company</li>
