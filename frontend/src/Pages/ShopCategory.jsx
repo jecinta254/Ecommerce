db1005f98feb1eb664981d32ada6d8fcdc8bb0a3
jecinta254 <njokijecinta254@gmail.com>
2024-08-07 05:09:41 +0300

    silvertech

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -9,7 +9,7 @@ export const ShopCategory = (props) => {
   return (
     <div className='shop-category'>
 
-    <img src={ props.banner } alt="" />
+    <img className='banners' src={ props.banner } alt="" />
     <div className="shopcategory-indexSort">
       <p>
         <span>Showing 1-12</span> out of 36 products
